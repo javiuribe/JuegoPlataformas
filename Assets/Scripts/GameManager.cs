78f013f2184fde992380411947934cc763d8ba6d
Javier <uribe.javier@gmx.es>
2025-01-04 17:31:32 +0100

    Animaciones y bugs arreglados

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,16 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI vidasTxt;
     public TextMeshProUGUI monedasTxt;
     public Personaje personaje;
+    public GameObject gameOverPn;
+    public TextMeshProUGUI recordTxt;
+    public TextMeshProUGUI monedasGameOverTxt;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,5 +22,14 @@ public class GameManager : MonoBehaviour
     {
         vidasTxt.text = personaje.Vidas.ToString();
         monedasTxt.text = personaje.monedas.ToString();
+        if (personaje.Vidas <=0 && !gameOverPn.activeSelf){
+            gameOverPn.SetActive(true);
+            recordTxt.text = PlayerPrefs.GetInt("monedas").ToString();
+            monedasGameOverTxt.text = personaje.monedas.ToString();
+        }
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene("Juego");
     }
 }
--- Assets/Scripts/Personaje.cs
+++ Assets/Scripts/Personaje.cs
@@ -5,48 +5,85 @@ public class Personaje : MonoBehaviour
 	private bool saltando;
 	public int Vidas{get;set;}
 	private bool atacando;
+	private bool moviendoDerecha;
+	private bool moviendoIzquierda;
 	public int monedas{get;set;}
 	public GameObject ataqueOriginal;
 	public GameObject ataquePosicion;
 	public Animator animator;
+	public AudioClip sonidoSalto;
+	public AudioClip sonidoAterrizaje;
+	private AudioSource personajeAudioSource;
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
 	{
 		Vidas = 3;
+		personajeAudioSource = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetKey(KeyCode.LeftArrow))
+		if (Input.GetKey(KeyCode.LeftArrow) || moviendoIzquierda)
 		{
-			transform.Translate(new Vector3(-0.1f, 0.0f));
+			MoverIzquierda();
 		}
-		else if (Input.GetKey(KeyCode.RightArrow))
+		else if (Input.GetKey(KeyCode.RightArrow) || moviendoDerecha)
 		{
-			transform.Translate(new Vector3(0.1f, 0.0f));
+			MoverDerecha();
 		}
-		if (!saltando && Input.GetKeyDown(KeyCode.UpArrow))
-		{
-			animator.SetTrigger("Jump");
-			animator.SetFloat("AirSpeedY", 1);
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 500.0f));
-			saltando = true;
-			animator.SetBool("Grounded", false);
-			Invoke("BajarVelocidadAire", 0.6f);
-		}
-		if (!atacando && Input.GetKeyDown(KeyCode.Space))
-		{
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+			Saltar();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Atacar();
+        }
+    }
+
+	public void setMoverIzquierda(bool activar){
+		moviendoIzquierda = activar;
+	}
+
+	public void setMoverDerecha(bool activar){
+		moviendoDerecha = activar;
+	}
+
+	public void MoverIzquierda(){
+		transform.Translate(new Vector3(-0.1f, 0.0f));
+	}
+
+	public void MoverDerecha(){
+		transform.Translate(new Vector3(0.1f, 0.0f));
+	}
+
+    public void Atacar()
+    {
+		if (!atacando){
 			GameObject.Instantiate(
-				ataqueOriginal, ataquePosicion.transform.position,
-				ataquePosicion.transform.rotation);
+							ataqueOriginal, ataquePosicion.transform.position,
+							ataquePosicion.transform.rotation);
 			Invoke("TerminarAtaque", 0.5f);
 			atacando = true;
 			animator.SetTrigger("Attack1");
 		}
-	}
+    }
 
-	private void TerminarAtaque()
+    public void Saltar()
+    {
+		if (!saltando){
+			animator.SetTrigger("Jump");
+			animator.SetFloat("AirSpeedY", 1);
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 450.0f));
+			saltando = true;
+			animator.SetBool("Grounded", false);
+			Invoke("BajarVelocidadAire", 0.6f);
+			personajeAudioSource.PlayOneShot(sonidoSalto);
+		}
+    }
+
+    private void TerminarAtaque()
 	{
 		atacando = false;
 	}
@@ -72,6 +109,7 @@ public class Personaje : MonoBehaviour
 		{
 			saltando = false;
 			animator.SetBool("Grounded", true);
+			personajeAudioSource.PlayOneShot(sonidoAterrizaje);
 		}
 		if (other.gameObject.tag == "Enemigo")
 		{
@@ -83,7 +121,7 @@ public class Personaje : MonoBehaviour
 			if (Vidas == 0)
 			{
 				Debug.Log("GAME OVER");
-				//gameObject.SetActive(false);
+				gameObject.SetActive(false);
 				animator.SetTrigger("Death");
 				int recordMonedas = PlayerPrefs.GetInt("monedas", 0);
 				if (monedas > recordMonedas)
